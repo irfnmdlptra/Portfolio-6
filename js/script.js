60d6269595228e530950ba6c392b3f4cb0f2c103
Irfnmdlptra <bangasrat@gmail.com>
2023-02-25 18:12:57 +0800

    faficon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let menu = document.querySelector('#menu-btn');
 let Close = document.querySelector('#nav-btn');
 let nav = document.querySelector('.nav-menu');
+// let navAtas = document.querySelectorAll('.nav-header');
+// let atas = document.querySelector('.atas');
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
@@ -34,6 +36,46 @@ themeToggler.onclick = () =>{
 
 
   
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+      atas.style.backgroundColor = '#e6e6e6';
+        for (let i=0; i <= navAtas.length; i++ ){
+          
+            navAtas[i].style.backgroundColor = 'transparent';
+            navAtas[i].style.Color = '#fff';
+        }
+    } else {
+      atas.style.backgroundColor = 'transparent';
+      atas.style.backdropFilter = 'none';
+    }
+  });
   
   
   
+
+
+  const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+
+  if (scrollY >= 100) {
+    box1.style.boxShadow = '0.4rem 0.4rem 1rem #111, -0.4rem -0.4rem 1rem #333';
+  } else {
+    box1.style.boxShadow = 'none';
+  }
+
+  if (scrollY >= 200) {
+    box2.style.boxShadow = '0.4rem 0.4rem 1rem #111, -0.4rem -0.4rem 1rem #333';
+  } else {
+    box2.style.boxShadow = 'none';
+  }
+
+  if (scrollY >= 300) {
+    box3.style.boxShadow = '0.4rem 0.4rem 1rem #111, -0.4rem -0.4rem 1rem #333';
+  } else {
+    box3.style.boxShadow = 'none';
+  }
+});
